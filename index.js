ce3cdc7b214bb3ce5a524981d0f18ea2e24af509
Zaneh Williams <zanehmw@gmail.com>
2016-05-31 20:11:21 -0400

    initial commit, added express

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+  res.send("Hello World")
+})
+
+app.listen(3001, function(){
+  console.log("It's aliiiiveee!")
+})
